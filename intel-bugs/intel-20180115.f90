26410b66d32143ce7d4829d6cb4da55aa111260c
Neil Carlson <neil.n.carlson@gmail.com>
2018-01-15 12:42:09 -0700

    New Intel bug

---
+++ intel-bugs/intel-20180115.f90
@@ -0,0 +1,53 @@
+!! Support request 03209754
+!!
+!! LENGTH PARAMETER NOT SET FOR POLYMORPHIC SELECT-TYPE SELECTOR
+!!
+!! In the following example a type bound function returning a pointer to an
+!! unlimited polymorphic variable is used as the selector in a SELECT TYPE
+!! construct.  When the dynamic type is CHARACTER, the length type of the
+!! selector is not being set correctly.
+!!
+!! $ ifort --version
+!! ifort (IFORT) 18.0.1 20171018
+!!
+!! $ ifort intel-20180115.f90
+!! $ ./a.out
+!! 0 ''
+!!
+!! The correct output is
+!! 5 'fubar'
+!!
+
+module foo_type
+
+  type :: foo
+    class(*), pointer :: uptr => null()
+  contains
+    procedure :: return_uptr
+  end type
+
+contains
+
+  function return_uptr(this) result(uptr)
+    class(foo) :: this
+    class(*), pointer :: uptr
+    uptr => this%uptr
+  end function
+
+end module
+
+
+program main
+
+  use foo_type
+  type(foo) :: x
+
+  allocate(x%uptr, source='fubar')
+
+  select type (uptr => x%return_uptr())
+  type is (character(*))
+    write(*,'(i0,1x,"''",a,"''")') len(uptr), uptr ! EXPECT 5 'fubar'
+  end select
+
+end program
+
