65fd511dcc4bdfaaa91a201e3e7c2fc848da310c
Neil Carlson <neil.n.carlson@gmail.com>
2018-01-17 11:55:48 -0700

    New Intel bug

---
+++ intel-bugs/intel-20180117.f90
@@ -0,0 +1,24 @@
+!! Support request 03212814
+!!
+!! Array bounds are not being set correctly in ALLOCATE with MOLD=
+!!
+!! Fortran 2008 standard, 6.7.1.2 item 6: "When an ALLOCATE statement is
+!! executed for an array with no allocate-shape-spec-list, the bounds of
+!! source-expr determine the bounds of the array."
+!!
+!! $ ifort --version
+!! ifort (IFORT) 18.0.1 20171018
+!!
+!! $ ifort intel-20180117.f90
+!! $ ./a.out
+!! 1 5
+!!
+!! The correct expected result is
+!! 5 9
+!!
+
+integer, pointer :: a(:), b(:)
+allocate(a(5:9))
+allocate(b, mold=a)
+print '(i0,1x,i0)', lbound(b), ubound(b)  ! EXPECT 5 9
+end
