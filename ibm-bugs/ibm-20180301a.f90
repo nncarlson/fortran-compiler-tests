78acda614fda519fc15134a5577e9f597d295db2
Neil Carlson <neil.n.carlson@gmail.com>
2018-03-01 10:31:14 -0700

    Adding IBM bugs

---
+++ ibm-bugs/ibm-20180301a.f90
@@ -0,0 +1,23 @@
+!!
+!! FUNCTION RESULT NOT RECOGNIZED TO HAVE BEEN SET
+!!
+!! The call to the intrinsic function C_F_POINTER sets the value of the
+!! function result but the xlf compiler fails to recognize this and gives
+!! a spurious compilation error
+!!
+!! $ xlf -qversion
+!! IBM XL Fortran for Linux, V15.1.6 (Community Edition)
+!! Version: 15.01.0006.0001
+!! 
+!! $ xlf2008 -c bug.f90
+!! "bug.f90", 1513-006 (E) FUNCTION or ENTRY result is not set in the program unit.
+!! ** f_string_pointer_aux   === End of Compilation 1 ===
+!! 1501-510  Compilation successful for file bug.f90.
+
+function f_string_pointer_aux(cptr, len) result(fptr)
+  use,intrinsic :: iso_c_binding
+  type(c_ptr), intent(in) :: cptr
+  integer(c_size_t), intent(in) :: len
+  character(len,kind=c_char), pointer :: fptr
+  call c_f_pointer(cptr, fptr)
+end function
---
+++ ibm-bugs/ibm-20180301b.f90
@@ -0,0 +1,45 @@
+!!
+!! INTERNAL COMPILER ERROR
+!!
+!! $ xlf2008 ibm-20180301b.f90 
+!! /projects/opt/ppc64le/ibm/xlf-15.1.6/xlf/15.1.6/bin/.orig/xlf2008: 1501-230 (S) Internal compiler error; please contact your Service Representative. For more information visit:
+!! http://www.ibm.com/support/docview.wss?uid=swg21110810
+!! 1501-511  Compilation failed for file ibm-20180301b.f90.
+!!
+
+program test_any_scalar_type
+
+  !call test_derived_type_value
+  !call test_assignment
+  !call test_shallow_assignment
+
+contains
+
+  subroutine test_derived_type_value
+    class(*), allocatable :: val
+    type point
+      !real x, y
+    end type
+    select type (val)
+    type is (point)
+    end select
+  end subroutine
+
+  subroutine test_assignment
+    class(*), allocatable :: val
+    type point
+      !real x, y
+    end type
+    select type (val)
+    type is (point)
+    end select
+  end subroutine
+
+  subroutine test_shallow_assignment
+    type box
+      !integer, pointer :: n => null()
+    end type
+    !type(box) :: b
+  end subroutine
+
+end program
