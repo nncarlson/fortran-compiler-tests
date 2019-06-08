9415fbedd670938519827124e45d7e599b4c83ad
Neil Carlson <neil.n.carlson@gmail.com>
2019-06-08 10:20:29 -0600

    New NAG test case for regression in 6232

---
+++ nag-bugs/nag-20190608.f90
@@ -0,0 +1,30 @@
+!!
+!! INTERNAL COMPILER ERROR
+!!
+!! This is a regression somewhere between 6228 (okay) to 6232 (ICE)
+!!
+!! $ nagfor -c nag-20190608.f90
+!! NAG Fortran Compiler Release 6.2(Chiyoda) Build 6232
+!! Panic: nag-20190608.f90: Unexpected expr node type 432
+!! Internal Error -- please report this bug
+!! Abort
+!!
+
+module foo
+
+  type :: ptr_box
+    real, pointer :: ptr(:) => null()
+  end type
+
+  type :: struct
+    type(ptr_box), allocatable :: array(:)
+  end type
+
+contains
+
+  subroutine bar(x, y)
+    type(struct) :: x, y
+    x%array(1)%ptr = y%array(1)%ptr
+  end subroutine
+
+end module
