0cea5aafe0a89cbc4022ab4deede5bad44af671c
Neil N. Carlson <neil.n.carlson@gmail.com>
2022-01-17 11:30:35 -0700

    Test case for NAG coarray bug

---
+++ nag-bugs/nag-20220117.f90
@@ -0,0 +1,18 @@
+!! SR108297
+!!
+!! INVALID INTERMEDIATE C CODE
+!!
+!! The NAG 7.1 compiler generates invalid intermediate C code for
+!! the intrinsic CO_BROADCAST subroutine below.
+!!
+!! $ nagfor -coarray -f2018 nag-20220117.f90 
+!! NAG Fortran Compiler Release 7.1(Hanzomon) Build 7102
+!! nag-20220117.f90: In function 'MAIN':
+!! nag-20220117.f90:15:142: error: '__NAGf90_CoDope2' has no member named 'dim'
+!!     4 | call co_broadcast(bsize, 1)
+
+integer, allocatable :: bsize(:)[:]
+allocate(bsize(5)[*])
+if (this_image() == 1) bsize(:)[1] = 1
+call co_broadcast(bsize, 1)
+end
