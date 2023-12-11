ca32e3a3b25178403c1043c560711f528e6cfaa6
Neil N. Carlson <neil.n.carlson@gmail.com>
2023-12-10 17:41:14 -0700

    Reproducer for Intel select rank bug

---
+++ intel-bugs/intel-20231210.f90
@@ -0,0 +1,24 @@
+!! REJECTS VALID CODE
+!!
+!! $ ifx --version
+!! ifx (IFX) 2024.0.0 20231017
+!!
+!! $ ifx intel-20231210.f90 
+!! intel-20231210.f90(20): error #6512: A scalar-valued expression is required in this context.   [X]
+!!         if (x) print *, 'pass'  ! X IS A SCALAR IN THIS RANK-CASE BLOCK
+!! ------------^
+!! compilation aborted for intel-20231210.f90 (code 1)
+
+call foo(.true.)
+contains
+  subroutine foo(x)
+    class(*), intent(in) :: x(..)
+    select rank (x)
+    rank (0)
+      select type (x)
+      type is (logical)
+        if (x) print *, 'pass'  ! X IS A SCALAR IN THIS RANK-CASE BLOCK
+      end select
+    end select
+  end subroutine
+end
