ffb031e091dedcbc3d66a129a8495447757d8eb0
Neil N. Carlson <neil.n.carlson@gmail.com>
2023-12-13 11:06:00 -0700

    Reproducer for possible Intel ifx bug
    Else an example of a poor quality implementation permitted
    by the standard.

---
+++ intel-bugs/intel-20231213.f90
@@ -0,0 +1,14 @@
+!!
+!! POOR OUTPUT WITH G0 REAL FORMATTING
+!!
+!! This may not be a bug at all, but rather merely what I consider to be a
+!! poor quality implementation choice permitted by the standard.
+!!
+!! With "-assume old_e0g0_format" ifx 2024.0 writes .5670000000000000E-13
+!! With "-assume noold_e0g0_format" (implied by -standard-semantics) it
+!! writes .6E-13
+!!
+
+use iso_fortran_env
+write(*,'(g0)') 5.67e-14_real64
+end
