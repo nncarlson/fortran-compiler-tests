04e5fd7d9b4ec9706d0e5a612fc05e5c74783410
Neil N. Carlson <neil.n.carlson@gmail.com>
2025-04-28 16:37:52 -0600

    New gfortran bug

---
+++ gfortran-bugs/gfortran-20250428.f90
@@ -0,0 +1,25 @@
+!! https://gcc.gnu.org/bugzilla/show_bug.cgi?id=119986
+!!
+!! INCORRECT RESULTS
+!!
+!! The correct real and imaginary part references of a complex array
+!! are not being passed to the subroutine.
+!!
+
+program main
+  type :: my_type
+    complex, allocatable :: phi(:)
+  end type
+  integer :: j
+  type(my_type) :: x
+  x%phi = [(cmplx(j,-j),j=1,4)]
+  call fubar(x%phi%re, x%phi%im)
+contains
+  subroutine fubar(u, v)
+    real, intent(in) :: u(:), v(:)
+    if (any(u /= [1,2,3,4])) stop 1
+    if (any(v /= -[1,2,3,4])) stop 2
+    !print *, 'phi%re:', u
+    !print *, 'phi%im:', v
+  end subroutine
+end program
