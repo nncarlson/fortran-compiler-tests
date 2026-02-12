82f3c0131098f75c22d31e064fab62a2e7a0d952
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-02-12 14:20:21 -0700

    New gfortran bug reproducer

--- gfortran-bugs/gfortran-20260212.f90
+++ gfortran-bugs/gfortran-20260212.f90
@@ -1,6 +1,6 @@
 !! https://gcc.gnu.org/bugzilla/show_bug.cgi?id=123899
 !!
-!! WRONG RESULT FROM SHAPE ON ASSUMED-RANK DUMMY AND 0-SIZED ACTUAL
+!! WRONG RESULT FROM SELECT-RANK ON ASSUMED-RANK DUMMY AND 0-SIZED ACTUAL
 !!
 !! Note that declaring a rank-1, 0-sized array and passing that will
 !! give the correct result. So there is something lacking in the
@@ -8,19 +8,20 @@
 !!
 !! $ gfortran --version
 !! GNU Fortran (GCC) 15.2.0
-!! $ gfortran gfortran-20260130.f90 
+!! $ gfortran gfortran-20260212.f90 
 !! $ ./a.out
 !! STOP 2
- 
-integer y(0)
-if (foo(y) /= 0) stop 1
-if (foo([integer::]) /= 0) stop 2
-if (foo(y-1) /= 0) stop 3
+
+call sub([integer::])
 contains
-  integer function foo(x) result(n)
+  subroutine sub(x)
     integer x(..)
-    integer dims(rank(x))
-    dims = shape(x)
-    n = dims(1)
-  end function
-end
+    if (rank(x) /= 1) stop 1
+    select rank (x)
+    rank (1)
+      ! should select here
+    rank default
+      stop 2
+    end select
+  end subroutine
+end 
